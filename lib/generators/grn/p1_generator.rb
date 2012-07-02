5458f7bbad05b34782b047bd1e98319411d3e475
Marcelo <marcelo.theodorojr@gmail.com>
2012-07-02 15:45:17 -0300

    test the variables are set

--- lib/generators/grn/p1_generator.rb
+++ lib/generators/grn/p1_generator.rb
@@ -3,6 +3,8 @@
 require 'rails/generators'
 require 'rails/generators/migration'
 
+require 'mdd/generators'
+
 module Grn
   module Generators
     class P1Generator < Rails::Generators::Base
@@ -11,22 +13,47 @@ module Grn
       
       source_root File.expand_path("../templates", __FILE__)
       
-      attr_accessor :resource_type_name, :resource_type_attributes, :resource_name, :resource_attributes
+      attr_accessor :resource_type_name, :resource_type_attributes, :resource_name, :resource_attributes, :resource, :resource_type
 
-      class_option :type_name, :desc => 'The name of the resource type', :type => :string
-      class_option :type_attributes, :desc => 'The name of the resource type', :type => :string
+      class_option :type_name, :desc => 'Resource type name', :type => :string
+      class_option :type_attributes, :desc => 'Resource type attributes', :type => :string
 
-      class_option :resource_name, :desc => 'The name of the resource type', :type => :string
-      class_option :resource_attributes, :desc => 'The name of the resource type', :type => :string
+      class_option :resource_name, :desc => 'Resource name', :type => :string
+      class_option :resource_attributes, :desc => 'Resource attributes', :type => :string
 
       def initialize(*args, &block)
 
         super
         
-        @resource_type_name = options.type_name || ask("Name of the resource type?\nExample: Category, ProductCategory...\n")
-        @resource_type_attributes = options.type_attributes || ask("What's the name of the resource type?")
+        # resource type
+        @resource_type_name = options.type_name || ask("Choose the resource type name.\nExample: Category, ProductCategory, etc.\nResource type name:")
+        @resource_type = Mdd::Generators::Model.new( @resource_type_name )
+        print_usage unless @resource_type.valid?
+        
+        #resource type attributes
+        @resource_type_attributes = options.type_attributes || ask("Choose the resource type attributes.\nSyntax: similar to MDD gem. Description is already included.\nResource type attributes:")
+        @resource_type_attributes.split(' ').each do |attribute|
+          @resource_type.add_attribute Mdd::Generators::ModelAttribute.new( attribute )
+        end
+
+        # resource 
+        @resource_name = options.resource_name || ask("Choose the resource name.\nExample: Product, Movie, Equipment, etc.\nResource name:")
+        @resource = Mdd::Generators::Model.new( @resource_name )
+        print_usage unless @resource.valid?
+        
+        # resource attributes
+        @resource_attributes = options.resource_attributes || ask("Choose the resource attributes.\nSyntax: similar to MDD gem. Description is already included.\nResource attributes:")
+        @resource_attributes.split(' ').each do |attribute|
+          @resource.add_attribute Mdd::Generators::ModelAttribute.new( attribute )
+        end
 
       end
+      
+      def log
+        
+        template 'test.rb', 'p1_generator.txt'
+        
+      end 
       
       # Implement the required interface for Rails::Generators::Migration.
       def self.next_migration_number(dirname)
